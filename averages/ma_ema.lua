cd39931c05d400f5e9cb000f86d20ee9c8f58df2
ninyflow14 <ninyflow14@gmail.com>
2024-06-24 20:37:01 -0600

    Update ma_ema.lua

--- averages/ma_ema.lua
+++ averages/ma_ema.lua
@@ -1,6 +1,6 @@
 instrument { name = "Exponential Moving Average", short_name = "EMA", overlay = true, icon="indicators:MA" }
 
-period = input (9, "front.period", input.integer, 1)
+period = input (14, "front.period", input.integer, 1)
 source = input (1, "front.ind.source", input.string_selection, inputs.titles_overlay)
 
 input_group {
